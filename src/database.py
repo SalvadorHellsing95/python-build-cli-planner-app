01af3b5112fc8b8bad8a658b11fd5aa2dd632564
Jose Salvador Gonzalez Blanco <salvador@MacBook-Air-de-Jose.local>
2022-01-05 12:37:28 -0600

    done with the project

--- src/database.py
+++ src/database.py
@@ -25,7 +25,6 @@ def add_reminder(text, date, ReminderClass):
     reminder = ReminderClass(text, date)
     if not isinstance(reminder, DeadlinedReminder):
         raise TypeError("Invalid Reminder Class")
-    reminder = ReminderClass(text, date)
 
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
--- src/reminder.py
+++ src/reminder.py
@@ -2,16 +2,18 @@ class PrefixedReminder:
     """This class acts as a base class for other types of reminders.
     Classes that subclass it should override the `self.text` property
     """
+
     def __init__(self, prefix="Hey, don't forget to "):
         self.prefix = prefix
         self.text = prefix + '<placeholder_text>'
 
-class PoliteReminder(PrefixedReminder):
 
+class PoliteReminder(PrefixedReminder):
     """Task 1"""
 
-    def __init__(self, text):
+    def __init__(self, text, date=None):
         super().__init__('please, ')
         self.text = self.prefix + text
 
-
+    def __iter__(self):
+        return iter([self.text])
