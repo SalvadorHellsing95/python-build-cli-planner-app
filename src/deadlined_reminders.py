3a3527c127af37b0a7fb2d048a3b3857690121ca
Jose Salvador Gonzalez Blanco <salvador@MacBook-Air-de-Jose.local>
2022-01-05 10:10:05 -0600

    completing task 4

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,9 +1,13 @@
-from abc import ABCMeta, abstractmethod
-
+from abc import ABCMeta, abstractmethod, ABC
 from collections.abc import Iterable
 
-
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
+
+    @abstractmethod
+    def is_due(self):
+        pass
+
+class DeadlinedReminder(Iterable, ABC):
 
     @abstractmethod
     def is_due(self):
